0401982a0468ca6070446b0764df581bd12921df
Craig Schneiderwent <cschneid-the-elder@users.noreply.github.com>
2022-05-21 23:07:53 -0500

    WIP cobol 202x environment division file-control paragraph

--- cobol/testdata/local/test9037.cbl
+++ cobol/testdata/local/test9037.cbl
@@ -39,6 +39,7 @@
              Assign To 'PILOT1' 'PILOT2' Using Zhaan
            Select Moya
              Assign Using Zhaan
+
            Select Moya
              Assign PILOT
              Access Sequential
@@ -90,6 +91,7 @@
            Select Moya
              Assign Using Zhaan
              Access Sequential
+
            Select Moya
              Assign PILOT
              Access Mode Sequential
@@ -243,6 +245,603 @@
            Select Moya
              Assign Using Zhaan
              Access Mode Is Sequential
+
+           Select Moya
+             Assign PILOT
+             Status Sikozu
+           Select Moya
+             Assign 'PILOT'
+             Status Sikozu
+           Select Moya
+             Assign PILOT1 PILOT2
+             Status Sikozu
+           Select Moya
+             Assign 'PILOT1' 'PILOT2'
+             Status Sikozu
+           Select Moya
+             Assign PILOT Using Zhaan
+             Status Sikozu
+           Select Moya
+             Assign 'PILOT' Using Zhaan
+             Status Sikozu
+           Select Moya
+             Assign PILOT1 PILOT2 Using Zhaan
+             Status Sikozu
+           Select Moya
+             Assign 'PILOT1' 'PILOT2' Using Zhaan
+             Status Sikozu
+           Select Moya
+             Assign To PILOT
+             Status Sikozu
+           Select Moya
+             Assign To 'PILOT'
+             Status Sikozu
+           Select Moya
+             Assign To PILOT1 PILOT2
+             Status Sikozu
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2'
+             Status Sikozu
+           Select Moya
+             Assign To PILOT Using Zhaan
+             Status Sikozu
+           Select Moya
+             Assign To 'PILOT' Using Zhaan
+             Status Sikozu
+           Select Moya
+             Assign To PILOT1 PILOT2 Using Zhaan
+             Status Sikozu
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2' Using Zhaan
+             Status Sikozu
+           Select Moya
+             Assign Using Zhaan
+             Status Is Sikozu
+           Select Moya
+             Assign PILOT
+             Status Is Sikozu
+           Select Moya
+             Assign 'PILOT'
+             Status Is Sikozu
+           Select Moya
+             Assign PILOT1 PILOT2
+             Status Is Sikozu
+           Select Moya
+             Assign 'PILOT1' 'PILOT2'
+             Status Is Sikozu
+           Select Moya
+             Assign PILOT Using Zhaan
+             Status Is Sikozu
+           Select Moya
+             Assign 'PILOT' Using Zhaan
+             Status Is Sikozu
+           Select Moya
+             Assign PILOT1 PILOT2 Using Zhaan
+             Status Is Sikozu
+           Select Moya
+             Assign 'PILOT1' 'PILOT2' Using Zhaan
+             Status Is Sikozu
+           Select Moya
+             Assign To PILOT
+             Status Is Sikozu
+           Select Moya
+             Assign To 'PILOT'
+             Status Is Sikozu
+           Select Moya
+             Assign To PILOT1 PILOT2
+             Status Is Sikozu
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2'
+             Status Is Sikozu
+           Select Moya
+             Assign To PILOT Using Zhaan
+             Status Is Sikozu
+           Select Moya
+             Assign To 'PILOT' Using Zhaan
+             Status Is Sikozu
+           Select Moya
+             Assign To PILOT1 PILOT2 Using Zhaan
+             Status Is Sikozu
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2' Using Zhaan
+             Status Is Sikozu
+           Select Moya
+             Assign Using Zhaan
+             Status Is Sikozu
+
+           Select Moya
+             Assign Using Zhaan
+             File Status Sikozu
+           Select Moya
+             Assign PILOT
+             File Status Sikozu
+           Select Moya
+             Assign 'PILOT'
+             File Status Sikozu
+           Select Moya
+             Assign PILOT1 PILOT2
+             File Status Sikozu
+           Select Moya
+             Assign 'PILOT1' 'PILOT2'
+             File Status Sikozu
+           Select Moya
+             Assign PILOT Using Zhaan
+             File Status Sikozu
+           Select Moya
+             Assign 'PILOT' Using Zhaan
+             File Status Sikozu
+           Select Moya
+             Assign PILOT1 PILOT2 Using Zhaan
+             File Status Sikozu
+           Select Moya
+             Assign 'PILOT1' 'PILOT2' Using Zhaan
+             File Status Sikozu
+           Select Moya
+             Assign To PILOT
+             File Status Sikozu
+           Select Moya
+             Assign To 'PILOT'
+             File Status Sikozu
+           Select Moya
+             Assign To PILOT1 PILOT2
+             File Status Sikozu
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2'
+             File Status Sikozu
+           Select Moya
+             Assign To PILOT Using Zhaan
+             File Status Sikozu
+           Select Moya
+             Assign To 'PILOT' Using Zhaan
+             File Status Sikozu
+           Select Moya
+             Assign To PILOT1 PILOT2 Using Zhaan
+             File Status Sikozu
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2' Using Zhaan
+             File Status Sikozu
+           Select Moya
+             Assign Using Zhaan
+             File Status Sikozu
+
+           Select Moya
+             Assign Using Zhaan
+             File Status Is Sikozu
+           Select Moya
+             Assign PILOT
+             File Status Is Sikozu
+           Select Moya
+             Assign 'PILOT'
+             File Status Is Sikozu
+           Select Moya
+             Assign PILOT1 PILOT2
+             File Status Is Sikozu
+           Select Moya
+             Assign 'PILOT1' 'PILOT2'
+             File Status Is Sikozu
+           Select Moya
+             Assign PILOT Using Zhaan
+             File Status Is Sikozu
+           Select Moya
+             Assign 'PILOT' Using Zhaan
+             File Status Is Sikozu
+           Select Moya
+             Assign PILOT1 PILOT2 Using Zhaan
+             File Status Is Sikozu
+           Select Moya
+             Assign 'PILOT1' 'PILOT2' Using Zhaan
+             File Status Is Sikozu
+           Select Moya
+             Assign To PILOT
+             File Status Is Sikozu
+           Select Moya
+             Assign To 'PILOT'
+             File Status Is Sikozu
+           Select Moya
+             Assign To PILOT1 PILOT2
+             File Status Is Sikozu
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2'
+             File Status Is Sikozu
+           Select Moya
+             Assign To PILOT Using Zhaan
+             File Status Is Sikozu
+           Select Moya
+             Assign To 'PILOT' Using Zhaan
+             File Status Is Sikozu
+           Select Moya
+             Assign To PILOT1 PILOT2 Using Zhaan
+             File Status Is Sikozu
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2' Using Zhaan
+             File Status Is Sikozu
+           Select Moya
+             Assign Using Zhaan
+             File Status Is Sikozu
+
+           Select Moya
+             Assign PILOT
+             Status Sikozu Jool
+           Select Moya
+             Assign 'PILOT'
+             Status Sikozu Jool
+           Select Moya
+             Assign PILOT1 PILOT2
+             Status Sikozu Jool
+           Select Moya
+             Assign 'PILOT1' 'PILOT2'
+             Status Sikozu Jool
+           Select Moya
+             Assign PILOT Using Zhaan
+             Status Sikozu Jool
+           Select Moya
+             Assign 'PILOT' Using Zhaan
+             Status Sikozu Jool
+           Select Moya
+             Assign PILOT1 PILOT2 Using Zhaan
+             Status Sikozu Jool
+           Select Moya
+             Assign 'PILOT1' 'PILOT2' Using Zhaan
+             Status Sikozu Jool
+           Select Moya
+             Assign To PILOT
+             Status Sikozu Jool
+           Select Moya
+             Assign To 'PILOT'
+             Status Sikozu Jool
+           Select Moya
+             Assign To PILOT1 PILOT2
+             Status Sikozu Jool
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2'
+             Status Sikozu Jool
+           Select Moya
+             Assign To PILOT Using Zhaan
+             Status Sikozu Jool
+           Select Moya
+             Assign To 'PILOT' Using Zhaan
+             Status Sikozu Jool
+           Select Moya
+             Assign To PILOT1 PILOT2 Using Zhaan
+             Status Sikozu Jool
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2' Using Zhaan
+             Status Sikozu Jool
+           Select Moya
+             Assign Using Zhaan
+             Status Is Sikozu Jool
+           Select Moya
+             Assign PILOT
+             Status Is Sikozu Jool
+           Select Moya
+             Assign 'PILOT'
+             Status Is Sikozu Jool
+           Select Moya
+             Assign PILOT1 PILOT2
+             Status Is Sikozu Jool
+           Select Moya
+             Assign 'PILOT1' 'PILOT2'
+             Status Is Sikozu Jool
+           Select Moya
+             Assign PILOT Using Zhaan
+             Status Is Sikozu Jool
+           Select Moya
+             Assign 'PILOT' Using Zhaan
+             Status Is Sikozu Jool
+           Select Moya
+             Assign PILOT1 PILOT2 Using Zhaan
+             Status Is Sikozu Jool
+           Select Moya
+             Assign 'PILOT1' 'PILOT2' Using Zhaan
+             Status Is Sikozu Jool
+           Select Moya
+             Assign To PILOT
+             Status Is Sikozu Jool
+           Select Moya
+             Assign To 'PILOT'
+             Status Is Sikozu Jool
+           Select Moya
+             Assign To PILOT1 PILOT2
+             Status Is Sikozu Jool
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2'
+             Status Is Sikozu Jool
+           Select Moya
+             Assign To PILOT Using Zhaan
+             Status Is Sikozu Jool
+           Select Moya
+             Assign To 'PILOT' Using Zhaan
+             Status Is Sikozu Jool
+           Select Moya
+             Assign To PILOT1 PILOT2 Using Zhaan
+             Status Is Sikozu Jool
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2' Using Zhaan
+             Status Is Sikozu Jool
+           Select Moya
+             Assign Using Zhaan
+             Status Is Sikozu Jool
+
+           Select Moya
+             Assign Using Zhaan
+             File Status Sikozu Jool
+           Select Moya
+             Assign PILOT
+             File Status Sikozu Jool
+           Select Moya
+             Assign 'PILOT'
+             File Status Sikozu Jool
+           Select Moya
+             Assign PILOT1 PILOT2
+             File Status Sikozu Jool
+           Select Moya
+             Assign 'PILOT1' 'PILOT2'
+             File Status Sikozu Jool
+           Select Moya
+             Assign PILOT Using Zhaan
+             File Status Sikozu Jool
+           Select Moya
+             Assign 'PILOT' Using Zhaan
+             File Status Sikozu Jool
+           Select Moya
+             Assign PILOT1 PILOT2 Using Zhaan
+             File Status Sikozu Jool
+           Select Moya
+             Assign 'PILOT1' 'PILOT2' Using Zhaan
+             File Status Sikozu Jool
+           Select Moya
+             Assign To PILOT
+             File Status Sikozu Jool
+           Select Moya
+             Assign To 'PILOT'
+             File Status Sikozu Jool
+           Select Moya
+             Assign To PILOT1 PILOT2
+             File Status Sikozu Jool
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2'
+             File Status Sikozu Jool
+           Select Moya
+             Assign To PILOT Using Zhaan
+             File Status Sikozu Jool
+           Select Moya
+             Assign To 'PILOT' Using Zhaan
+             File Status Sikozu Jool
+           Select Moya
+             Assign To PILOT1 PILOT2 Using Zhaan
+             File Status Sikozu Jool
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2' Using Zhaan
+             File Status Sikozu Jool
+           Select Moya
+             Assign Using Zhaan
+             File Status Sikozu Jool
+
+           Select Moya
+             Assign Using Zhaan
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign PILOT
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign 'PILOT'
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign PILOT1 PILOT2
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign 'PILOT1' 'PILOT2'
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign PILOT Using Zhaan
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign 'PILOT' Using Zhaan
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign PILOT1 PILOT2 Using Zhaan
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign 'PILOT1' 'PILOT2' Using Zhaan
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign To PILOT
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign To 'PILOT'
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign To PILOT1 PILOT2
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2'
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign To PILOT Using Zhaan
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign To 'PILOT' Using Zhaan
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign To PILOT1 PILOT2 Using Zhaan
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2' Using Zhaan
+             File Status Is Sikozu Jool
+           Select Moya
+             Assign Using Zhaan
+             File Status Is Sikozu Jool
+
+           Select Moya
+             Assign PILOT
+             Password Noranti
+           Select Moya
+             Assign 'PILOT'
+             Password Noranti
+           Select Moya
+             Assign PILOT1 PILOT2
+             Password Noranti
+           Select Moya
+             Assign 'PILOT1' 'PILOT2'
+             Password Noranti
+           Select Moya
+             Assign PILOT Using Zhaan
+             Password Noranti
+           Select Moya
+             Assign 'PILOT' Using Zhaan
+             Password Noranti
+           Select Moya
+             Assign PILOT1 PILOT2 Using Zhaan
+             Password Noranti
+           Select Moya
+             Assign 'PILOT1' 'PILOT2' Using Zhaan
+             Password Noranti
+           Select Moya
+             Assign To PILOT
+             Password Noranti
+           Select Moya
+             Assign To 'PILOT'
+             Password Noranti
+           Select Moya
+             Assign To PILOT1 PILOT2
+             Password Noranti
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2'
+             Password Noranti
+           Select Moya
+             Assign To PILOT Using Zhaan
+             Password Noranti
+           Select Moya
+             Assign To 'PILOT' Using Zhaan
+             Password Noranti
+           Select Moya
+             Assign To PILOT1 PILOT2 Using Zhaan
+             Password Noranti
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2' Using Zhaan
+             Password Noranti
+           Select Moya
+             Assign Using Zhaan
+             Password Noranti
+
+           Select Moya
+             Assign PILOT
+             Password Is Noranti
+           Select Moya
+             Assign 'PILOT'
+             Password Is Noranti
+           Select Moya
+             Assign PILOT1 PILOT2
+             Password Is Noranti
+           Select Moya
+             Assign 'PILOT1' 'PILOT2'
+             Password Is Noranti
+           Select Moya
+             Assign PILOT Using Zhaan
+             Password Is Noranti
+           Select Moya
+             Assign 'PILOT' Using Zhaan
+             Password Is Noranti
+           Select Moya
+             Assign PILOT1 PILOT2 Using Zhaan
+             Password Is Noranti
+           Select Moya
+             Assign 'PILOT1' 'PILOT2' Using Zhaan
+             Password Is Noranti
+           Select Moya
+             Assign To PILOT
+             Password Is Noranti
+           Select Moya
+             Assign To 'PILOT'
+             Password Is Noranti
+           Select Moya
+             Assign To PILOT1 PILOT2
+             Password Is Noranti
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2'
+             Password Is Noranti
+           Select Moya
+             Assign To PILOT Using Zhaan
+             Password Is Noranti
+           Select Moya
+             Assign To 'PILOT' Using Zhaan
+             Password Is Noranti
+           Select Moya
+             Assign To PILOT1 PILOT2 Using Zhaan
+             Password Is Noranti
+           Select Moya
+             Assign To 'PILOT1' 'PILOT2' Using Zhaan
+             Password Is Noranti
+           Select Moya
+             Assign Using Zhaan
+             Password Is Noranti
+
+           Select Moya
+             Assign PILOT
+             Organization Sequential
+           Select Moya
+             Assign PILOT
+             Organization Is Sequential
+           Select Moya
+             Assign PILOT
+             Organization Record Sequential
+           Select Moya
+             Assign PILOT
+             Organization Line Sequential
+           Select Moya
+             Assign PILOT
+             Organization Is Record Sequential
+           Select Moya
+             Assign PILOT
+             Organization Is Line Sequential
+
+           Select Moya
+             Assign PILOT
+             Record Delimiter Standard-1
+           Select Moya
+             Assign PILOT
+             Record Delimiter Stark
+           Select Moya
+             Assign PILOT
+             Record Delimiter Is Standard-1
+           Select Moya
+             Assign PILOT
+             Record Delimiter Is Stark
+
+           Select Moya
+             Assign PILOT
+             Reserve 42
+           Select Moya
+             Assign PILOT
+             Reserve 42 Area
+           Select Moya
+             Assign PILOT
+             Reserve 42 Areas
+
+           Select Moya
+             Assign PILOT
+             Padding Jothee
+           Select Moya
+             Assign PILOT
+             Padding Is Jothee
+           Select Moya
+             Assign PILOT
+             Padding Character Jothee
+           Select Moya
+             Assign PILOT
+             Padding Character Is Jothee
+           Select Moya
+             Assign PILOT
+             Padding x'42'
+           Select Moya
+             Assign PILOT
+             Padding Is x'42'
+           Select Moya
+             Assign PILOT
+             Padding Character x'42'
+           Select Moya
+             Assign PILOT
+             Padding Character Is x'42'
            .
        Procedure Division.
            GOBACK.
